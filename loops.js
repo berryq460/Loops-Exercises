7f09edd5fe7ef84d1642c8c7699099ed7bbadcbe
Quiana Berry <berry.quiana@gmail.com>
2020-09-29 23:24:55 -0400

    loop classwork

--- loops.js
+++ loops.js
@@ -4,13 +4,41 @@
 //Bonus: Use a nested for loop to show the tables for every
 //multiplier from 1 to 10 (100 results total)
 
+var multiplier = 9;
+for (var i = 0; i <= 10; i++) {
+    var result = multiplier * i;
+    console.log(multiplier + ' * ' + i + ' = ' + result);
+}
+for (var multiplier = 0; multiplier <= 10; multiplier++) {
+ for (var i = 0; i <= 10; i++) {
+   var result = multiplier * i;
+   console.log(multiplier + ' * ' + i + ' = ' + result);
+ }
+}
+
 //Write a while loop that starts counting from 5.
 //Count up to (including) 50 and count only every fifth number
 //Don' forget to log it
 
+
 //Write a function that takes an array of numbers as an argument.
 //This function should return the first duplicate number in the array:
 // For example: [2,7,5,9,8,7] should return 7;
+
+let testArr = [2, 7, 5, 9, 8, 7];
+function findDuplicate(arr)
+{
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                return arr[i];
+            }
+        }
+    }
+    return -1; // if theres no d
+}
+console.log(findDuplicate(testArr));
+
 
 //Remembering the results from the conditionals exercise on Monday
 // use the assignGrade function by logging every value from 60 to 100:
